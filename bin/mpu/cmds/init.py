76edc1c66548456787f4744f0a31a45bf864ff88
ljcucc <ljcucc@gmail.com>
2022-04-21 02:18:58 +0800

    fix naming system by adding random chars

--- bin/mpu/cmds/init.py
+++ bin/mpu/cmds/init.py
@@ -3,11 +3,17 @@ from os import path
 import json
 
 from ..command import Command, CmdInfo
+from ..mp_controller import MPController
+
+import random
+import string
+rand_char = lambda y: ''.join(random.choice(string.ascii_letters) for x in range(y))
 
 class InitCommand(Command):
   def __init__(self):
     super().__init__(CmdInfo("init", "create a VM instance for current folder"))
     self.config = {}
+    self.mpc = MPController()
 
   def run(self):
     self.read_config()
@@ -27,15 +33,24 @@ class InitCommand(Command):
   def prompt_config(self):
     current_folder = os.getcwd()
     config = {
+      "version": 1,
       "name": os.path.basename(current_folder).lower().replace("_","-"),
-      "disk": "10G"
+      "disk": "10G",
+      "dir": current_folder
     }
-    config['name'] = input(f"environment name: ({config['name']}) ") or config['name']
-    config['disk'] = input(f"disk size: ({config['disk']}) ") or config['disk']
+    config['name'] = input(f"* environment name: ({config['name']}) ") or config['name']
+    while True:
+      rid = rand_char(6)
+      if(not rid+config['name'] in self.mpc.list_vm()):
+        config['name'] = rand_char(6)+"-"+config['name']
+        break
 
+    config['disk'] = input(f"* disk size: ({config['disk']}) ") or config['disk']
+
+    print()
     print(config)
 
-    confirm = input("Is this OK? (yes) ")
+    confirm = input("\nIs this OK? (yes) ")
     if not (len(confirm) > 0 and (confirm[0].lower() == 'y' or confirm[0] == '\n')):
       print("canceled")
       exit(1)
@@ -61,4 +76,4 @@ class InitCommand(Command):
       with open(config_path) as jsonf:
         self.config = json.load(jsonf)
     
-    self.read_options()
+    # self.read_options()
