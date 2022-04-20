93c7ed55438fe1c64b2d1eee50e4e40a50f55bbe
ljcucc <ljcucc@gmail.com>
2022-04-21 02:18:37 +0800

    create MPController

--- bin/mpu/mp_controller.py
+++ bin/mpu/mp_controller.py
@@ -1,5 +1,25 @@
-from mimetypes import init
+from dataclasses import dataclass
+import os
+import subprocess
+import json
+
+from dataclasses import dataclass
+
+@dataclass
+class InstanceInfo:
+  name: str
+  disk: str
 
 class MPController:
   def __init__(self) -> None:
-      pass
+    pass
+  
+  def list_vm(self):
+    jsons = str(subprocess.run(["multipass", "list", "--format","json"], stdout=subprocess.PIPE).stdout.decode("utf-8"))
+    vms = []
+
+    obj = json.loads(jsons)
+    for vm in obj["list"]:
+      vms.append(vm["name"])
+    
+    return vms
