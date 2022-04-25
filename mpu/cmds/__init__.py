09697748ee28c278a7402eb740965a4e5abe64a1
ljcucc <ljcucc@gmail.com>
2022-04-25 22:52:22 +0800

    simplify import

---
+++ mpu/cmds/__init__.py
@@ -0,0 +1,10 @@
+from mpu.cmds.init import InitCommand
+from mpu.cmds.pkg import PkgCommand
+from mpu.cmds.info import InfoCommand
+from mpu.cmds.rm import RemoveCommand
+from mpu.cmds.exec import ExecCommand, RunCommand
+from mpu.cmds.doctor.doctor import DoctorCommand
+from mpu.cmds.help import HelpCommand
+from mpu.cmds.config import *
+from mpu.cmds.stop import *
+from mpu.cmds.start import *
