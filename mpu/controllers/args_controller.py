bcc9b57bc8c88e041ce1c7a9c5fa993d9b8571cd
ljcucc <ljcucc@gmail.com>
2022-04-25 13:41:18 +0800

    add pydocs and down_level check

--- mpu/controllers/args_controller.py
+++ mpu/controllers/args_controller.py
@@ -2,15 +2,23 @@ import sys
 import copy
 
 class ArgsController:
+  """
+  Args Controller:
+  
+  + args: str - all args
+  + name: str - name of the args (or command)
+  + title() -> str - return the first args, as title
+  """
   def __init__(self) -> None:
     self.args = copy.deepcopy(sys.argv)
+    self.name = self.args[0]
     del self.args[0]
 
     self.levels = []
     self.parse()
   
   def isValidCmd(self):
-    print(self.levels)
+    # print(self.levels)
     return len(self.levels) > 0
   
   def parse(self):
@@ -22,10 +30,14 @@ class ArgsController:
       self.levels.append(head)
   
   def command_head(self):
-    return self.levels[0]
+    return "" if len(self.levels) ==0 else self.levels[0]
   
   def title(self): return self.command_head()
   
   def down_level(self):
+    if(len(self.levels) == 0):
+      return self
     obj = copy.deepcopy(self)
     del obj.levels[0]
+
+    return obj
