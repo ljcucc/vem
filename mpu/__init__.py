7d538cd3e786d3f564ae58a03538eb331928d444
ljcucc <ljcucc@gmail.com>
2022-04-25 22:57:35 +0800

    simplify import globally

---
+++ mpu/__init__.py
@@ -0,0 +1,7 @@
+from mpu.command import CmdInfo, Command, CommandManager
+
+from mpu.cmds import *
+from mpu.controllers import *
+
+from mpu.controllers.engine.mp_controller import InstanceInfo, MPController
+from mpu.controllers.engine.docker_controller import *
