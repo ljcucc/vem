d88d1f8188b105b48f3a816245b34bb4ce3e52f7
ljcucc <ljcucc@gmail.com>
2022-04-25 13:59:09 +0800

    adding docs to MPController

--- mpu/mp_controller.py
+++ mpu/mp_controller.py
@@ -13,20 +13,27 @@ class InstanceInfo:
   disk: str
 
 class MPController:
+  """AKA, MultiPass-Controller"""
   def __init__(self) -> None:
     pass
 
   def cmd_run_bg(self,cmds):
+    """run command without showing output and return output"""
     return str(subprocess.run(cmds.split(" "), stdout=subprocess.PIPE).stdout.decode("utf-8"))
 
   def cmd_run(self,cmds):
+    """run command without hidding output (and no returning output)"""
     return str((subprocess.run(cmds.split(" ")).stdout or b"").decode("utf-8"))
 
   def cmd_run_disp(self,cmds):
+    """run command and display it as a format: 
+      CMD: [cmd_str]
+    """
     print("CMD: ", cmds)
     self.cmd_run(cmds)
   
   def list_vm(self):
+    """return all instances list object"""
     vms = []
 
     obj = json.loads(self.cmd_run_bg("multipass list --format json"))
@@ -36,14 +43,21 @@ class MPController:
     return vms
   
   def info_vm(self, name):
+    """print info of a instance"""
     self.cmd_run(f"multipass info {name}")
   
   def remove_vm(self, name):
+    """remove vm instance"""
     self.cmd_run(f"multipass delete {name}")
     self.cmd_run(f"multipass purge")
 
+  def exec_vm(self, name):
+    """run a command to a instance"""
+
   def exists(self, name):
+    """check a instance is exists or not by using name of it"""
     return name in self.list_vm()
 
   def launch_vm(self, config:ConfigData):
+    """launch command alt of multipass"""
     self.cmd_run_disp(f"multipass launch --name {config.name} --disk {config.disk}")
