ff2d9ef07f709c4cfcf10b6dcc401bf449960a24
ljcucc <ljcucc@gmail.com>
2022-04-25 15:31:32 +0800

    init PackagesControllers

---
+++ mpu/controllers/PackagesControllers.py
@@ -0,0 +1,9 @@
+class PackagesController:
+  """A controller to install, uninstall package by using PMController"""
+  def __init__(self) -> None:
+    pass
+
+class PMController:
+  """PackageManagerController: A controller to run package manager commands"""
+  def __init__(self) -> None:
+    pass
