5e111b5c2d0d516b51fd6b94dcffc13cdd1d9601
ljcucc <ljcucc@gmail.com>
2022-04-25 14:28:17 +0800

    apply disp_help from CommandManager

--- mpu/cmds/pkg.py
+++ mpu/cmds/pkg.py
@@ -2,7 +2,28 @@ import os
 from os import path
 
 from mpu.controllers.args_controller import ArgsController
-from ..command import Command, CmdInfo
+from ..command import Command, CmdInfo, CommandManager
+
+class UninstallCommand(Command):
+  """'pkg uninstall'"""
+  def __init__(self) -> None:
+    super().__init__(
+      CmdInfo("uninstall", "Uninstall a package from instance")
+    )
+
+class InstallCommand(Command):
+  """'pkg install'"""
+  def __init__(self) -> None:
+    super().__init__(
+      CmdInfo("install", "Install a package to instance")
+    )
+
+class ListCommand(Command):
+  """'pkg list'"""
+  def __init__(self) -> None:
+      super().__init__(
+        CmdInfo("list", "List all installed packages on a instance")
+      )
 
 class PkgCommand(Command):
   """Command 'pkg' class"""
@@ -11,15 +32,14 @@ class PkgCommand(Command):
     super().__init__(
       CmdInfo("pkg", "manage package of VM instance")
     )
-  
-  def disp_help(self):
-    """return help docs string"""
 
-    return """
-using 
-    """
- 
+    self.cm = CommandManager([
+      UninstallCommand(),
+      InstallCommand()
+    ])
+  
   def run(self, args: ArgsController):
+    """while pkg command executed"""
     if not args.isValidCmd():
-      print(self.disp_help())
+      print(self.cm.disp_help(usage="""vem pkg <command> [...options]"""))
     func = args.down_level().title()
