1de308dc3e01c312ce29dd23b772b937bc5d07a0
ljcucc <ljcucc@gmail.com>
2022-04-25 15:17:49 +0800

    finish basic install and uninstall function

--- mpu/cmds/pkg.py
+++ mpu/cmds/pkg.py
@@ -2,7 +2,9 @@ import os
 from os import path
 
 from mpu.controllers.args_controller import ArgsController
+from mpu.controllers.config_controller import ConfigController
 from ..command import Command, CmdInfo, CommandManager
+from mpu.mp_controller import MPController
 
 class UninstallCommand(Command):
   """'pkg uninstall'"""
@@ -10,6 +12,20 @@ class UninstallCommand(Command):
     super().__init__(
       CmdInfo("uninstall", "Uninstall a package from instance")
     )
+    self.mpc = MPController()
+    self.cc = ConfigController()
+
+  def run(self, args: ArgsController):
+    pkg_name = args.title()
+    config = self.cc.getConfig()
+    pm_cli = config.pm_cli
+
+    # TODO: currently only support apt-get, this will merge into PMController
+    self.mpc.exec(config.name, f"sudo {pm_cli} -y uninstall {pkg_name}")
+    self.mpc.exec(config.name, f"sudo {pm_cli} autoremove")
+
+    config.packages.remove(pkg_name)
+    self.cc.setConfig(config)
 
 class InstallCommand(Command):
   """'pkg install'"""
@@ -17,6 +33,21 @@ class InstallCommand(Command):
     super().__init__(
       CmdInfo("install", "Install a package to instance")
     )
+    self.mpc = MPController()
+    self.cc = ConfigController()
+  
+  def run(self, args: ArgsController):
+    pkg_name = args.title()
+    config = self.cc.getConfig()
+    pm_cli = config.pm_cli
+
+    # TODO: currently only support apt-get, this will merge into PMController
+    self.mpc.exec(config.name, f"sudo {pm_cli} update")
+    self.mpc.exec(config.name, f"sudo {pm_cli} -y install {pkg_name}")
+
+    config.packages.append(pkg_name)
+    print(config)
+    self.cc.setConfig(config)
 
 class ListCommand(Command):
   """'pkg list'"""
@@ -42,4 +73,6 @@ class PkgCommand(Command):
     """while pkg command executed"""
     if not args.isValidCmd():
       print(self.cm.disp_help(usage="""vem pkg <command> [...options]"""))
-    func = args.down_level().title()
+      return False
+    # print(args.title())
+    self.cm.exec(args.title(), args.down_level())
